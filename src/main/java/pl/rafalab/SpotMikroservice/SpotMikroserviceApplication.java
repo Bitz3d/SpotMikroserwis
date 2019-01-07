b6e960074122a5d17c54e94c58c0001c7ffcfb8a
Bitz3d <raf.cieslinski@gmail.com>
2019-01-07 21:15:12 +0100

    first commit

---
+++ src/main/java/pl/rafalab/SpotMikroservice/SpotMikroserviceApplication.java
@@ -0,0 +1,14 @@
+package pl.rafalab.SpotMikroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMikroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotMikroserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/rafalab/SpotMikroservice/SpotMikroserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.rafalab.SpotMikroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotMikroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
